a4bae0b24a553ac98e3e32b343769a4f349c38a1
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-16 00:03:12 +0200

    Done Towels-sort(reverse)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = [];
+    if (matrix == undefined || matrix == 0) {
+        return [];
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        if (i > 0 && i % 2 !== 0) {
+            let rev = matrix[i].reverse();
+            for(let item = 0; item < rev.length; item++) {
+                result.push(rev[item]);
+            }
+        } else {
+            let part = matrix[i];
+            for(let n = 0; n < part.length; n++) {
+                result.push(part[n]);
+            }
+        }
+    }
+    return result;
 }
